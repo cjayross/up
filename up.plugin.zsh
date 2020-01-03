f3f69f42ee3c0394e49ec08fb95392ac61d27791
cjayross <calvinjayross@gmail.com>
2020-01-02 23:22:31 -0600

    fixed formatting

--- up.plugin.zsh
+++ up.plugin.zsh
@@ -9,7 +9,7 @@ _up() {
             compadd -qS / -a - args
         fi
     }
-_sequence -s / _path_seq
+    _sequence -s / _path_seq
 }
 
 up() {
@@ -19,45 +19,45 @@ up() {
         echo 'usage: up [integer | directory]'
     }
 
-while [[ $1 =~ ^- && ! $1 == '--' ]]; do
-    case $1 in
-        --help | -h)
-            print_help
-            return 0
-            ;;
-        *)
-            echo 'up: '$1': unknown option.'
-            print_help
-            return 1
-            ;;
-    esac
-    shift
-done
+    while [[ $1 =~ ^- && ! $1 == '--' ]]; do
+        case $1 in
+            --help | -h)
+                print_help
+                return 0
+                ;;
+            *)
+                echo 'up: '$1': unknown option.'
+                print_help
+                return 1
+                ;;
+        esac
+        shift
+    done
 
-if (( $# > 1 )); then
-    echo 'up: trailing arguments.'
-    return 1
-fi
-
-if [ -z $1 ]; then
-    cd ..
-elif [[ $1 =~ ^[0-9]+$ ]]; then
-    dir=$PWD`echo $(yes '/..' | head -n $1) | sed 's/ //g'`
-    cd $dir
-elif [[ $1 == / || $1 =~ $HOME && $PWD =~ $HOME ]]; then
-    cd $1
-else
-    base=${1%%/*}
-    if [[ ! $PWD =~ /$base(/|$) ]]; then
-        echo 'up: '$1': could not find directory in $PWD.'
+    if (( $# > 1 )); then
+        echo 'up: trailing arguments.'
         return 1
     fi
-    dir=${PWD%$base*}$1
-    cd $dir &>/dev/null
-    if [[ $? != 0 ]]; then
-        echo 'up: '$dir': no such directory.'
+
+    if [ -z $1 ]; then
+        cd ..
+    elif [[ $1 =~ ^[0-9]+$ ]]; then
+        dir=$PWD`echo $(yes '/..' | head -n $1) | sed 's/ //g'`
+        cd $dir
+    elif [[ $1 == / || $1 =~ $HOME && $PWD =~ $HOME ]]; then
+        cd $1
+    else
+        base=${1%%/*}
+        if [[ ! $PWD =~ /$base(/|$) ]]; then
+            echo 'up: '$1': could not find directory in $PWD.'
+            return 1
+        fi
+        dir=${PWD%$base*}$1
+        cd $dir &>/dev/null
+        if [[ $? != 0 ]]; then
+            echo 'up: '$dir': no such directory.'
+        fi
     fi
-fi
 }
 
 compdef _up up
